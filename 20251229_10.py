3a608356af104a11de14cc3c45ac4ac89cc709b3
Gyeongdaeyoung <86515840+rudeodud@users.noreply.github.com>
2025-12-30 08:21:48 +0900

    Update 20251229_10.py

--- 20251229_10.py
+++ 20251229_10.py
@@ -1,2 +1,13 @@
+def get_peri():
+    radius = 5.0
+    result = radius * radius * 3.141592
+    print("get_peri() = " + result)
+
+def get_pero(radius):
+    result = radius * radius * 3.141592
+    print("get_peri({})")
+
+
 def main():
     radius = float(input("원의 반지름을 입력하시오:"))
+    
